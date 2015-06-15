aaeba5dc14cc0c20c49175da13f7c042757f83c8
emmont <emmon0@qq.com>
2015-06-15 18:09:13 +0800

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,39 @@
-require 'rails_helper'
+require 'spec_helper'
 
-describe "Home page" do
-	it "should have the content 'Sample App'"do
-		dr = Selenium::WebDriver.for:ie
-		url = 'www.baidu.com'
-		dr.get url
-		#~ dr.find_element(:id,'su').should be_displayed
-		expect(dr.find_element(:id,'su')).to be_displayed#新的语法
-	end #it
-end #describe
+describe "Static pages" do
+	subject { page }
+	describe "Home page" do
+		before { visit root_path }
+		it { should have_content('Sample App') }
+		it { should have_title(full_title('')) }
+		it { should_not have_title('| Home') }
+		endrequire 'spec_helper'
+	end	
+
+	describe "Static pages" do
+		subject { page }
+		describe "Home page" do
+			before { visit root_path }
+			it { should have_content('Sample App') }
+			it { should have_title(full_title('')) }
+			it { should_not have_title('| Home') }
+	end
+
+	describe "Help page" do
+		before { visit help_path }
+		it { should have_content('Help') }
+		it { should have_title(full_title('Help')) }
+	end
+
+	describe "About page" do
+		before { visit about_path }
+		it { should have_content('About') }
+		it { should have_title(full_title('About Us')) }
+		end
+	
+	describe "Contact page" do
+		before { visit contact_path }
+		it { should have_content('Contact') }
+		it { should have_title(full_title('Contact')) }
+	end
+end
